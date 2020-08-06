90a2aeeebc3d7b38cc5d201d61f3eb80eed119f0
Clark Kenneth Racadio <kennethracadio@hotmail.com>
2020-08-06 09:24:38 -0400

    Add source icons and compile for v1.19.0 (gesture icons)

---
+++ components/GesturePlus.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const SvgGesturePlus = (props?: React.SVGProps<SVGSVGElement>) => (
+  <svg width={6} height={6} fill="none" {...props}>
+    <path
+      d="M2.296 6h1.408V3.661H6V2.339H3.704V0H2.296v2.339H0v1.322h2.296V6z"
+      fill="#fff"
+    />
+  </svg>
+);
+
+export default SvgGesturePlus;
---
+++ dist/GestureScroll.d.ts
@@ -0,0 +1,3 @@
+import * as React from "react";
+declare const SvgGestureScroll: (props?: React.SVGProps<SVGSVGElement>) => JSX.Element;
+export default SvgGestureScroll;
